4d0df671caa303d90def0b2baee62a58355fc034
Brandon Locke <blocke92@gmail.com>
2020-09-10 15:54:47 -0400

    created predictor.py with pseudo code

---
+++ predictor.py
@@ -0,0 +1,21 @@
+import sys
+
+
+#establish db connection
+
+def main(args):
+  #load in schedule data from database as list: games = []
+  
+  #load in probability data (if not random)
+  
+  #loop through until max runs is reached (froms args)
+  #on each loop, simulate each game and place results back into object
+  #write results to database
+  
+  #print descriptive statistics and exit
+  #runtime
+  #number of runs
+
+
+if __name__ == "__main__":
+  main(sys.argv)
